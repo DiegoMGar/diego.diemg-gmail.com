547ce6f78830b93218ac675d2f2ce4b2df81f258
Diego Maroto <diego.diemg@gmail.com>
2020-04-03 12:42:35 +0200

    commands, new deploy, new service and how to expose it

---
+++ commands/minikube-docker-start.sh
@@ -0,0 +1,2 @@
+# Add this flag: --docker-opt bip=172.18.0.1/16 to your minikube start command.
+minikube start --docker-opt bip=172.18.0.1/16
---
+++ commands/start-nginx-app.sh
@@ -0,0 +1,2 @@
+kubectl create -f deployments/nginx-deploy.yml
+kubectl create -f services/service-definition.yml
